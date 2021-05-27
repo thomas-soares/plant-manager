8d27c0425e4f0673da7fe0d8be8478e8bdb465e5
Thomas Soares <thomas__soares@hotmail.com>
2021-05-26 23:14:09 -0300

    Colocando imagem de background.

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import wateringImg from '../assets/watering.png';
+
 export default function Welcome() {
   return (
     <View>
       <Text>
         Gerencie suas plantas de forma fácil
       </Text>
+      <Image source={imgWatering} />
     </View>
   );
 }
